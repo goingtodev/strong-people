7dce42da95d6a79193da3dc76022a11468f04f78
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-01-16 14:33:36 +0900

    chore: 협업 전 환경 및 Route 설정

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('/join.html')
+# Join
+@app.route('/join', method=["POST"])
+def join_post():
+    return render_template('/index.html')
+
+# Home
+
+#Detail
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
+
